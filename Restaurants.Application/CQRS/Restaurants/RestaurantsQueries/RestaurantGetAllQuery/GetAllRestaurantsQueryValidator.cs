59ee7a7d7bdc1d07cc6ed13f562d6b96e7f96de0
reza <rezajafarpoor222@gmail.com>
2024-07-08 18:59:10 +0330

    pagination validator implemented

---
+++ Restaurants.Application/CQRS/Restaurants/RestaurantsQueries/RestaurantGetAllQuery/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.CQRS.Restaurants.RestaurantsQueries.RestaurantGetAllQuery;
+
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private int[] _allowPageSizes = [5, 10, 15, 30];
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber)
+            .GreaterThanOrEqualTo(1);
+        RuleFor(r => r.PageSize)
+            .Must(value => _allowPageSizes.Contains(value))
+            .WithMessage($"page size must be in [ {string.Join(",", _allowPageSizes)} ]");
+    }
+    
+}
