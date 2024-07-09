ac872005358deae465e9e1f761d8ff11b49ac294
reza <rezajafarpoor222@gmail.com>
2024-07-10 03:29:01 +0330

    unit test for updateCommand validator

---
+++ Restaurants.Application.Tests/UnitTests/CommandValidator/UpdateRestaurantCommandValidatorTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.TestHelper;
+using Restaurants.Application.CQRS.Restaurants.RestaurantsCommands.UpdateRestaurantCommand;
+
+namespace Restaurants.Application.Tests.UnitTests.CommandValidator;
+
+public class UpdateRestaurantCommandValidatorTest
+{
+    [Fact]
+    public void Validator_shouldHaveNoErrors_withValidCommand()
+    {
+        // Arrange
+        var command = new UpdateRestaurantCommand(1)
+        {
+            Name = "test",
+            Description = "not null"
+        };
+        // Act
+
+        var sut = new UpdateRestaurantCommandValidator();
+        var result = sut.TestValidate(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+    [Fact]
+    public void Validator_shouldHaveErrors_withInValidCommand()
+    {
+        // Arrange
+        var command = new UpdateRestaurantCommand(1)
+        {
+            Name = "tt"
+        };
+        // Act
+
+        var sut = new UpdateRestaurantCommandValidator();
+        var result = sut.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x =>x.Description)
+            .WithErrorMessage("Description is required");
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+
+    }
+}
---
+++ Restaurants.Application.Tests/UnitTests/MappingTests/CreateRestaurantCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Restaurants.Application.CQRS.Restaurants.RestaurantsCommands.RestaurantCreateCommand;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.Tests.UnitTests.MappingTests;
+
+public class CreateRestaurantCommandTests
+{
+
+
+    [Fact]
+    public void FromEntity_ShouldReturnARestaurant_WhenCommandExist()
+    {
+        
+        // Arrange
+        var command = new CreateRestaurantCommand
+        {
+            Name= "restaurant", 
+            Description = "description",
+            Category = "Italian",
+            ContactEmail = "test@test.com",
+            City = "city",
+            PostalCode = "12-345",
+            Street = "street",
+            HasDelivery = true,
+            Id = 1
+        };
+        
+        // Act
+        var restaurant = CreateRestaurantCommand.FromEntity(command);
+        
+
+        // Assert
+        restaurant.Should().BeOfType<Restaurant>();
+        restaurant.Address.Should().BeOfType<Address>();
+
+    }
+}
