0d2666c0d0b5c3c63d7774eecf797e7c668d4989
reza <rezajafarpoor222@gmail.com>
2024-07-10 03:39:21 +0330

    unit test for GetAll restaurant validator added

---
+++ Restaurants.Application.Tests/UnitTests/CommandValidator/GetAllRestaurantsQueryValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation.TestHelper;
+using Restaurants.Application.Common;
+using Restaurants.Application.CQRS.Restaurants.RestaurantsQueries.RestaurantGetAllQuery;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.Tests.UnitTests.CommandValidator;
+
+public class GetAllRestaurantsQueryValidatorTests
+{
+    [Fact]
+    public void Validator_ShouldHaveNoErrors_WithValidCommand()
+    {
+     
+        // Arrange
+        
+        var command = new GetAllRestaurantsQuery(
+            null, 15, 4, null, SortDirection.Ascending);
+        
+        // Act
+        
+        var sut = new GetAllRestaurantsQueryValidator();
+        var result = sut.TestValidate(command);
+        
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+        
+    }
+    [Fact]
+    public void Validator_ShouldHaveErrors_WithInValidCommand()
+    {
+     
+        // Arrange
+         int[] _allowPageSizes = [5, 10, 15, 30];
+         string[] _allowSortByColumns =
+             [nameof(Restaurant.Category), nameof(Restaurant.Name), nameof(Restaurant.Description)];
+
+        var command = new GetAllRestaurantsQuery(
+            null, 100, -1, nameof(Restaurant.HasDelivery), SortDirection.Ascending);
+        
+        // Act
+        
+        var sut = new GetAllRestaurantsQueryValidator();
+        var result = sut.TestValidate(command);
+        
+        // Assert
+        result.ShouldHaveValidationErrorFor(x =>x.PageNumber);
+        result.ShouldHaveValidationErrorFor(x =>x.PageSize)
+            .WithErrorMessage($"page size must be in [ {string.Join(",", _allowPageSizes)} ]");
+        result.ShouldHaveValidationErrorFor(x => x.SortBy)
+            .WithErrorMessage($"page size must be in [ {string.Join(",", _allowSortByColumns)} ]");
+
+
+    }
+}
