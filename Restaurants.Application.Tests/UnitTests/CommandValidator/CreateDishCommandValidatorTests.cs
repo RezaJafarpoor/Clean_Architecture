6d436a6c1116d712a851bdd2241a8a3631ddac79
reza <rezajafarpoor222@gmail.com>
2024-07-10 03:45:22 +0330

    unit test for Create dish validator

---
+++ Restaurants.Application.Tests/UnitTests/CommandValidator/CreateDishCommandValidatorTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation.TestHelper;
+using Restaurants.Application.CQRS.Dishes.DishesCommands.CreateDishCommand;
+
+namespace Restaurants.Application.Tests.UnitTests.CommandValidator;
+
+public class CreateDishCommandValidatorTests
+{
+
+    [Fact]
+    public void Validator_ShouldHaveNoError_WithValidCommand()
+    {
+        
+        // Arrange
+        var command = new CreateDishCommand
+        {
+            Price = 10,
+            KiloCalories = 10
+        
+        };
+        var validator = new CreateDishCommandValidator();
+        // Act
+        var result = validator.TestValidate(command);
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+    [Fact]
+    public void Validator_ShouldHaveError_WithInValidCommand()
+    {
+        
+        // Arrange
+        var command = new CreateDishCommand
+        {
+            Price = -1,
+            KiloCalories = -10
+        
+        };
+        var validator = new CreateDishCommandValidator();
+        // Act
+        var result = validator.TestValidate(command);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Price)
+            .WithErrorMessage("Price must be a non negative number");
+        result.ShouldHaveValidationErrorFor(x => x.KiloCalories)
+            .WithErrorMessage("KiloCalories must be a non negative number");
+    }
+}
